52b9310faefbebfbbf73981a5d1e7f9265993586
Christoph Mellauner <c.mellauner@gmail.com>
2022-10-08 18:55:05 +0200

    small fixes

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ function installDependencies ()
 function installOhMyZsh ()
 {
   echo "Installing OhMyZsh"
+  rm -rf ~/.oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   echo "finished installing"
