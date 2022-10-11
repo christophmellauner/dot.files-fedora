1c47462a770f59e2005d9905f4d12fcfa5856b8b
Christoph Mellauner <c.mellauner@gmail.com>
2022-10-11 00:37:22 -0500

    add new config stuff

--- install.sh
+++ install.sh
@@ -55,10 +55,19 @@ function installFlatpakPackages ()
   flatpak -y install $(cat flatpak-package-list.txt | tr "\n" " ")
 }
 
+function installVSCode ()
+{
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf -y check-update
+  sudo dnf -y install code
+}
+
 installDependencies
 installDocker
 installDDEV
 installOhMyZsh
 installSpacevim
 installNVM
+installVSCode
 installFlatpakPackages
